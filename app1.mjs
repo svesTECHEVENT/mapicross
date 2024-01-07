9ae9d35baff8b46d1635e7f1107985ad66390545
svesTECHEVENT <144025248+svesTECHEVENT@users.noreply.github.com>
2024-01-06 19:53:43 -0800

    Add files via upload

---
+++ app1.mjs
@@ -0,0 +1,38 @@
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import http from 'http';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+// Define the CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
+
+// Define the Swiggy API proxy route
+app.use('/api/proxy/swiggy/mapi', createProxyMiddleware({
+  target: 'https://www.swiggy.com',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api/proxy/swiggy/mapi': '/mapi',
+  },
+}));
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
